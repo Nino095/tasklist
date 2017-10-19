922c69a0091fe16965bfeb1f96ed574d4a9274a7
Nino Bicanic <ninobicanic7@gmail.com>
2017-10-19 22:26:03 +0200

    napravljen master blade app

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="hr">
+	<head>
+		<title>Tasks app - Laravel vježba</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<div class="container">
+		<nav class="navbar navbar-default">
+		</nav>
+		<!-- Navbar sadržaj -->
+		</div>
+		@yield('content')
+	</body>
+</html>
