169b603e2acb715ddc06c617430b725cf8ec9661
Nino Bicanic <ninobicanic7@gmail.com>
2017-10-19 22:15:50 +0200

    napravljen kontroler za  rutu novi task

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,36 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+// Prikaz svih taskova u bazi
 Route::get('/', function () {
-    return view('welcome');
+    //return view('tasks');
+});
+
+//Dodavanje novog taska
+Route::post('/task', function (Request $request) {
+	$validator = Validator::make($request->all(), [
+		'name' => 'required|max:255'
+	]);
+	
+	if ($validator->fails()) {
+		return redirect('/')->withInput ()->withErrors($validator);
+	}
+	
+	// stvaranje Taska
+	$task = new Task;
+	$task->name = $request->name;
+	$tastk->save();
+	
+	return redirect('/');
+	
+});
+
+// Brisanje postojećeg taska
+Route::delete('/task/{id}',function ($id) {
+	// kod za brisanje taska
+});
+
+// Prikaz određenog taska
+Route::get('/task/(id)', function ($id) {
+	//kod za prikaz taska
 });
